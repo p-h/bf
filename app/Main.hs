66918b235ad47c7720985513e432ee1b48339ac2
Philippe Hürlimann <p@hurlimann.org>
2020-02-21 18:44:22 +0100

    Disable io buffering

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,16 @@ module Main where
 import           Lib
 
 import           System.Environment             ( getArgs )
+import           System.IO                      ( BufferMode(..)
+                                                , hSetBuffering
+                                                , stdout
+                                                , stdin
+                                                )
 
 main :: IO ()
 main = do
+    hSetBuffering stdin  NoBuffering
+    hSetBuffering stdout NoBuffering
     (filename : _) <- getArgs
     file           <- readFile filename
     inp            <- getContents
